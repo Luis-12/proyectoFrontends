048a69bca042acb6abbec99653137902fbeb2e22
Luis Alfredo Vargas Alvarado <55066676+Luis-12@users.noreply.github.com>
2021-10-12 19:45:06 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const artistasApi = "https://artista-server.herokuapp.com";
+const entity= "artista";
+
+
+function getAll() {
+  fetch(artistasApi + '/' + entity)
+  .then(response => response.json())
+  .then((data) => {
+    fetch('../templates/list.html')
+    .then((response) => response.text())
+    .then((template) => {
+      var rendered = Mustache.render(template, data);
+      document.getElementById('content').innerHTML = rendered;    
+   });
+  })
+}
+
+function checkId(item) {
+	return item._id==this
+}
+
+function getById(query) {
+  fetch(artistasApi + '/' + entity +'/' + query.id)
+    .then(response => response.json())
+    .then((data) => {
+      fetch('/template/detail.html')
+        .then((response) => response.text())
+        .then((template) => {
+          let rendered = Mustache.render(template, data);
+          document.getElementById('content').innerHTML = rendered;
+        });
+    })
+}
+
+function init() {
+  getAll();
+  router = new Navigo(null, false, '#!');
+	router.on({
+	  '/get': function(_,query) {
+		 getById(query);
+	  }
+	});
+	router.on(() => getAll());
+	router.resolve();
+}
